adccbd7d9674d2c9b9f00841f1d4b9d3790361e9
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-26 23:21:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,9 @@
 def my_each(array)# put argument(s) here
-  array 
+  counter = 0 
+  while counter < array.length 
+  yield(array[counter])
+  counter = counter + 1 
+end 
+array 
   # code here
 end
